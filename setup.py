6bb2a42836f32a46cc078377280863a99803b7f7
Sergey Chuvakin <sergeychuvakin1@mail.ru>
2017-12-23 16:56:25 +0300

    Implement a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+
+setup (name='spss_pandas', 
+       version="0.0.1",
+       description = 'SPSS to Pandas', 
+       author = 'I',
+       author_email = 'sergeychuvakin1@mail.ru',
+       license = 'MIT',
+       packages = ['spss_pandas'], 
+       zip_safe = True) 
